a02fb85704a33cd15b5529a3a7bfbf8e14609d92
w33bvGL <140258231+w33bvGL@users.noreply.github.com>
2025-01-27 16:31:30 +0000

    Apply automatic changes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,8 +11,5 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-    public function run(): void
-    {
-
-    }
+    public function run(): void {}
 }
